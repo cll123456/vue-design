d1c3ffd70f694e3a987ff9d1f2b0154492d22045
cll123456 <2642137848@qq.com>
2022-04-11 10:49:10 +0800

    chore(package.json): 🐳 更新包的内容

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
+import components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 自动引入组件
+    components({
+      dirs: ['src/components'],
+      resolvers: [ElementPlusResolver()],
+      dts: './src/components.d.ts',
+    }),
+    // 自动引入库的api
+    AutoImport({
+      imports: ['vue', 'pinia'],
+      dts: './src/auto-imports.d.ts',
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'), //设置别名
+    },
+    dedupe: ['vue'],
+  },
 })
